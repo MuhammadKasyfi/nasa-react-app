63df39c9c1ac660cab341f663d2510f5e9b8ac19
Kasyfi <kasyfikh@gmail.com>
2025-08-16 14:11:02 +0800

    finish

---
+++ src/components/SideBar1.jsx
@@ -0,0 +1,19 @@
+export default function SideBar() {
+    return (
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Brutal Martian Landscape</h2>
+                    <div>
+                        <p>Description</p>
+                        <p>dasdadwadadwwddwa saddasdada dasdadadsdasdasd
+                            dasdadadad
+                        </p>
+                    </div>
+                    <button>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+            </div>
+    )
+}
